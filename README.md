0009630ae2a53926760d0f8170a6d8c68249eaa8
dbremer-tonn <daniel.bremer-tonn@akquinet.de>
2011-01-10 15:04:01 +0100

    initial version of javascript-quickstart archetype

---
+++ README.md
@@ -0,0 +1,32 @@
+javascript-archetypes
+=====================
+
+This projects provides several Maven archetypes for Javascript. Those archetypes allows to quickly bootstrap a Maven project
+to develop an javascript application. The created projects automatically:
+
+* running jsunit tests
+* creating jsdocs
+* running jslint
+* creating compressed versions of all your js and css files
+
+
+The javascript-quickstart archetype
+--------------------------------
+The quickstart archetype creates a simple javascript application.
+
+    mvn archetype:generate \
+      -DarchetypeArtifactId=javascript-quickstart \
+      -DarchetypeGroupId=de.akquinet.javascript.archetypes \
+      -DarchetypeVersion=0.0.1-SNAPSHOT \
+      -DgroupId=your.company \
+      -DartifactId=javascript-quickstart-application
+
+
+Once generated, the application is ready to be built.
+-----------------------------------------------------
+The javascript-archetype-project is an open source project licensed under the Apache License 2.0.
+It is founded by [akquinet](http://akquinet.de/en)
+
+Technical Notes
+---------------
+* As the archetypes used the new archetype format, they don't work with the deprecated goal 'archetype:create'
---
+++ javascript-quickstart/src/main/resources/archetype-resources/src/main/javascript/quickstart.js
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2010 akquinet
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @fileOverview A simple Quickstart javascript example
+ * @version ${project.version}
+ */
+
+
+/**
+ * DE_AKQUINET_QUICKSTART package declaration.
+ * This declaration makes sure to not erase the
+ * current declaration. If the package does not
+ * exist an empty object is created.
+ * @default {}
+ */
+var DE_AKQUINET = DE_AKQUINET || {};
+
+/**
+ * A quickstart object
+ * @namespace
+ */
+DE_AKQUINET.quickstart = function () {
+
+    /**
+     * Public interface
+     * @scope DE_AKQUINET.quickstart
+     */
+    return {
+
+        /**
+         * Adds two numbers ...
+         * @param number1 first number
+         * @param number2 second number
+         * @return the sum of the two numbers
+         */
+        addNumbers : function(number1, number2) {
+            return number1 + number2;
+        }
+    };
+} ();
